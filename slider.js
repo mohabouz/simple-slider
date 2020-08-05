c2dd09aa94b9bf168daefba15f0ad1c5fb404acd
mohabouz <bouzidmohamed7@gmail.com>
2020-08-06 00:30:11 +0100

    Enhanced sliding mechanisms and fixed bugs

--- slider.js
+++ slider.js
@@ -1,34 +1,78 @@
 var leftBtn = document.getElementById("left"),
     righBtn = document.getElementById("right"),
     sldierContainer = document.getElementById("slider_container"),
-    sliderItem =  document.getElementsByClassName("slider_item");
+    sliderItem =  document.getElementsByClassName("slider_item"),
+	circlesContainer = document.getElementsByClassName("cercles")[0];
+
+var goRightInterval = setInterval(goRight,7000);
+var circElements = [];
+var counter = 0;
+var slidesNum = sliderItem.length;
+
+sldierContainer.style.width = (slidesNum*900) + "px";
+
+for (var i = 0 ; i < sliderItem.length ; i++) {
+	var elem = document.createElement("span");
+	elem.setAttribute("data-circ", i);
+	circlesContainer.appendChild(elem);
+	elem.addEventListener("click", function(){
+		var n = this.dataset.circ;
+		goTo(n);
+	});
+	circElements.push(elem);
+}
+
+circElements[0].className = "active";
+
+function goTo(n) {
+	sldierContainer.style.marginLeft = -n*900 + "px";
+	counter = n;
+	refreshCircles();
+	resetInterval();
+}
 
 function goLeft (){
-    if (parseInt(sldierContainer.style.marginLeft) == 0){
-      sldierContainer.style.marginLeft = -(sliderItem.length-1)*100 + "%";
+    if (counter == 0){
+      sldierContainer.style.marginLeft = -(slidesNum-1)*900 + "px";
+	  counter = slidesNum - 1;
     } else {
-      sldierContainer.style.marginLeft = (parseInt(sldierContainer.style.marginLeft)+100) + "%";
+      sldierContainer.style.marginLeft = -900*(counter-1) + "px";
+	  counter--;
     }
+	
+	refreshCircles();
 }
 
 function goRight (){
-    if (-parseInt(sldierContainer.style.marginLeft) >= (sliderItem.length-1)*100){
-      sldierContainer.style.marginLeft = 0;
+	if (counter == slidesNum-1){
+		sldierContainer.style.marginLeft = 0;
+		counter = 0;
     } else {
-      sldierContainer.style.marginLeft = (parseInt(sldierContainer.style.marginLeft)-100) + "%";
+		sldierContainer.style.marginLeft = -900*(counter+1) + "px";
+		counter++;
     }
+	
+	refreshCircles();
 }
 
-var goRightInterval = setInterval(goRight,7000);
-
 leftBtn.addEventListener('click',function(){
-  goLeft();
-  clearInterval(goRightInterval);
-  goRightInterval = setInterval(goRight,7000);
+	goLeft();
+	resetInterval();
 });
 
 righBtn.addEventListener('click',function(){
-  goRight();
-  clearInterval(goRightInterval);
-  goRightInterval = setInterval(goRight,7000);
+	goRight();
+	resetInterval();
 });
+
+function resetInterval() {
+	clearInterval(goRightInterval);
+	goRightInterval = setInterval(goRight,7000);
+}
+
+function refreshCircles(){
+	for(var i = 0 ; i < circElements.length ; i++) {
+		circElements[i].className = (i == counter ? "active" : "");
+	}
+}
+
